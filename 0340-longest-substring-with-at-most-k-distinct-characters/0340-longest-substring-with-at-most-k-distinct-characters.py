651f12865bfb6e2f8274b536b72bdfeb1571a760
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2022-10-26 13:14:22 -0400

    Time: 207 ms (27.73%), Space: 14.3 MB (43.58%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -4,15 +4,13 @@ class Solution:
         window_start = 0
         length = 0
         for window_end in range(len(s)):
-            seen[s[window_end]] = seen.get(s[window_end], 0) + 1
-            if len(seen) <= k:
-                length = max(length, window_end - window_start + 1)
-            
+            seen[s[window_end]] = seen.get(s[window_end], 0) + 1        
             while len(seen) > k:
                 seen[s[window_start]] -= 1
                 if seen[s[window_start]] == 0:
                     del seen[s[window_start]]
                 window_start += 1
+            length = max(length, window_end - window_start + 1)
         return length
                     
                 
