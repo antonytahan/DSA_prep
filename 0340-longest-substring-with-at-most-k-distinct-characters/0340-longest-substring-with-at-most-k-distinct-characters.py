1d2f70c78a20e9bc17d874cea16f5988d4652a28
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2022-11-04 11:25:42 -0400

    Time: 265 ms (13.59%), Space: 14.5 MB (10.07%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,18 +1,36 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        # seen = {}
+        # window_start = 0
+        # length = 0
+        # for window_end in range(len(s)):
+        #     seen[s[window_end]] = seen.get(s[window_end], 0) + 1        
+        #     while len(seen) > k:
+        #         seen[s[window_start]] -= 1
+        #         if seen[s[window_start]] == 0:
+        #             del seen[s[window_start]]
+        #         window_start += 1
+        #     length = max(length, window_end - window_start + 1)
+        # return length
+        
         seen = {}
-        window_start = 0
+        windowStart = 0
         length = 0
-        for window_end in range(len(s)):
-            seen[s[window_end]] = seen.get(s[window_end], 0) + 1        
+        #loop using windowEnd:
+        for windowEnd in range(len(s)):
+            # add to hash map and increase counter
+            seen[s[windowEnd]] = seen.get(s[windowEnd], 0) + 1
+            #if we have k distinct chars, shrink length
             while len(seen) > k:
-                seen[s[window_start]] -= 1
-                if seen[s[window_start]] == 0:
-                    del seen[s[window_start]]
-                window_start += 1
-            length = max(length, window_end - window_start + 1)
+                seen[s[windowStart]] -= 1
+                # delete from hash map if no more occurrences in string
+                if seen[s[windowStart]] == 0:
+                    del seen[s[windowStart]]
+                windowStart += 1
+            # update length of longest substring
+            length = max(length, windowEnd - windowStart + 1)
         return length
-                    
+
                 
             
         
