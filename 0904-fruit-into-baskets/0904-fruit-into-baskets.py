81b2af24fbd4afcbe9c5e019199d8f004b799e30
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2022-11-04 11:50:36 -0400

    Time: 2597 ms (24.09%), Space: 19.8 MB (87.86%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,16 +1,24 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        # initialize variables and sliding window
         seen = {}
         window_start = 0
         fruits_picked = 0
+        # initialize window_end as for loop iterator
         for window_end in range(len(fruits)):
+            # add vals to dictionary
             seen[fruits[window_end]] = seen.get(fruits[window_end], 0) + 1
+            # if we exceeded the limit, shrink window
             while len(seen) > 2:
                 seen[fruits[window_start]] -= 1
                 if seen[fruits[window_start]] == 0:
                     del seen[fruits[window_start]]
+                # increment start of window
                 window_start += 1
-            fruits_picked = max(fruits_picked, sum(seen.values()))
+            # get max of self and the number of fruits in baskets within limit
+            #fruits_picked = max(fruits_picked, sum(seen.values()))
+            fruits_picked = max(fruits_picked, window_end - window_start + 1)
+
         return fruits_picked
         
         
