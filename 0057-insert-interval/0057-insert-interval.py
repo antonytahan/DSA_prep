16ccc3b9c3652ccf6c213d6d2cdb69266d7705c3
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2022-11-09 21:17:54 -0500

    Time: 195 ms (31.69%), Space: 17.4 MB (53.30%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,99 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+#         res = []
+        
+#         for i in range(len(intervals)):
+#             if newInterval[1] < intervals[i][0]:
+#                 res.append(newInterval)
+#                 return res + intervals[i:]
+#             elif newInterval[0] > intervals[i][1]:
+#                 res.append(intervals[i])
+#             else:
+#                 newInterval[0] = min(newInterval[0], intervals[i][0])
+#                 newInterval[1] = max(newInterval[1], intervals[i][1])
+#         res.append(interval)
+#         return res
+
+
+        #second method, greedy:
+#         index, n = 0, len(intervals)
+#         res = []
+#         while index < n and newInterval[0] > intervals[index][0]:
+#             res.append(intervals[index])
+#             index += 1
+#         if not res or res[-1][1] < newInterval[0]:
+#             res.append(newInterval)
+#         else:
+#             res[-1][1] = max(res[-1][1], newInterval[1])
+        
+#         while index < n:
+#             if res[-1][1] < intervals[index][0]:
+#                 res.append(intervals[index])
+#             else:
+#                 res[-1][1] = max(res[-1][1], intervals[index][1])
+#             index += 1
+                
+#         return res
+    
+        #3rd method, really neat
+        #Collect the intervals strictly left or right of the new interval, then merge the new one with the middle ones (if any) before inserting it between left and right ones.
+
+        # s, e = newInterval
+        # left = [i for i in intervals if i[1] < s]
+        # right = [i for i in intervals if i[0] > e]
+        # if left + right != intervals:
+        #     s = min(s, intervals[len(left)][0])
+        # #~len(right) basically returns -len(right) - 1 
+        #     e = max(e, intervals[~len(right)][1])
+        # return left + [[s,e]] + right
+        
+        
+        
+#-----revisit -------
+#         index, n = 0, len(intervals)
+#         res = []
+#         while index < n and newInterval[0] > intervals[index][0]:
+#             res.append(intervals[index])
+#             index += 1
+#         if not res or res[-1][1] < newInterval[0]:
+#             res.append(newInterval)
+#         else:
+#             res[-1][1] = max(res[-1][1], newInterval[1])
+        
+#         while index < n:
+#             if res[-1][1] >= intervals[index][0]:
+#                 res[-1][1] = max(res[-1][1], intervals[index][1])
+#             else:
+#                 res.append(intervals[index])
+#             index += 1
+#         return res
+
+
+        res = []
+        i = 0
+        #add to output all intervals that are before the new interval
+        while i < len(intervals) and intervals[i][0] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        # add newInterval into res -- either by appending or merging
+        if not res or res[-1][1] < newInterval[0]:
+            res.append(newInterval)
+        else:
+            res[-1][1] = max(res[-1][1], newInterval[1])
+            
+        #add remaining intervals to output
+        while i < len(intervals):
+            # if no overlap, simply append
+            if res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])         
+            # otherwise, merge
+            else:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            i += 1
+        return res
+            
+
+            
+            
+        
