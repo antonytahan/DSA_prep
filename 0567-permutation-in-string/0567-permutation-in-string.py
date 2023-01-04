3a8f514b40d3b7282ebd5c41a136dfadacd1f154
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-04 15:22:15 -0500

    Time: 373 ms (30.88%), Space: 14.3 MB (7.47%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        word_dict = {}
+        for c in s1:
+            word_dict[c] = word_dict.get(c, 0) + 1
+        
+        matched, l = 0, 0
+        for r in range(len(s2)):
+            print(l, r)
+            if s2[r] in word_dict:
+                word_dict[s2[r]] -= 1
+                if word_dict[s2[r]] == 0:
+                    matched += 1
+            
+            if matched == len(word_dict):
+                return True
+            
+            if r  - l + 1 >= len(s1):
+                if s2[l] in word_dict:
+                    if word_dict[s2[l]] == 0:
+                        matched -= 1
+                    word_dict[s2[l]] += 1
+                l += 1
+        return False
+            
+
+
+            
