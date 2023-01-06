a432ba5fd1d8b08872921dffeafe52f956aa8c2f
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-06 14:39:14 -0500

    Time: 76 ms (77.12%), Space: 26 MB (85.53%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,15 +1,22 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
+        # define variables
+        # defined both as set to not add repeated substrings and O(1) lookup
         seen = set()
         res = set()
+        # iterate over each letter until the last possible valid substring(10 from end)
         for l in range(len(s) - 10 + 1):
-            if s[l : l + 10] in seen and s[l : l + 10] not in res:
-                res.add(s[l : l + 10])
-            else:
-                seen.add(s[l : l + 10])
+            # define end of string window size
+            r = l + 10
+            # if substring has been seen and not in the res set add it
+            if s[l : r] in seen and s[l : r] not in res:
+                res.add(s[l : r])
+            # otherwise add to seen if not there
+            elif s[l : r] not in seen:
+                seen.add(s[l : r])
+        # need to return solution in list form
         return list(res)
-            
-        
-        
-        
+
+
+
