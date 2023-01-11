db3fe6d59ba680d0d3b5e5af6dc7c06677af9bf3
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-11 17:57:57 -0500

    Time: 225 ms (86.78%), Space: 16.3 MB (47.88%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,18 +1,41 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        l, r = 0, len(nums) - 1
-        highest_sq_i = len(nums) - 1
+        # # initialize a results list
+        # res = [0] * len(nums)
+        # # initialize two pointers
+        # l, r = 0, len(nums) - 1
+        # # initialize index to populate with squares
+        # highest_sq_i = len(nums) - 1
+        # # <= because we need to handle the last number too
+        # while l <= r:
+        #     left_sq = nums[l] * nums[l]
+        #     right_sq = nums[r] * nums[r]
+        #     # if the left larger, put that into the index and shift l
+        #     if left_sq > right_sq:
+        #         res[highest_sq_i] = left_sq
+        #         l += 1
+        #     else:
+        #         # otherwise, populate with right and shift r by 1
+        #         res[highest_sq_i] = right_sq
+        #         r -= 1
+        #     # after populate index, shift index
+        #     highest_sq_i -= 1
+        # return res
+        
+        
+# --- revisit ----
+        l = 0
+        r = len(nums) - 1
+        res = []
         while l <= r:
-            print(l, r)
             left_sq = nums[l] * nums[l]
             right_sq = nums[r] * nums[r]
-            if left_sq > right_sq:
-                res[highest_sq_i] = left_sq
-                l += 1
-            else:
-                res[highest_sq_i] = right_sq
+            if left_sq < right_sq:
+                res.append(right_sq)
                 r -= 1
-            highest_sq_i -= 1
-        return res
+            else:
+                res.append(left_sq)
+                l += 1
+        return res[::-1]
+            
         
