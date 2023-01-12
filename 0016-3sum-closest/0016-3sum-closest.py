63cb29d790f84ef2a7d4c2d851ea672033c08b19
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-12 14:38:28 -0500

    Time: 1221 ms (55.23%), Space: 13.9 MB (94.06%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,30 +1,46 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        # nums.sort() 
+        # closest = float('inf')
+        # res = 0
+        # for i in range(len(nums) - 2):
+        #     l = i + 1
+        #     r = len(nums) - 1
+        #     while l < r:
+        #         if abs(nums[i] + nums[l] + nums[r] - target) < closest:
+        #             closest = abs(nums[i] + nums[l] + nums[r] - target)
+        #             res = nums[i] + nums[l] + nums[r]
+        #         if nums[i] + nums[l] + nums[r] < target:
+        #             l += 1
+        #         elif nums[i] + nums[l] + nums[r] > target:
+        #             r -= 1
+        #         else:
+        #             res = target
+        #             break
+        # return res
+
+        #sort in order to implement two pointer appraoch
         nums.sort()
-        #adding two checks for leetcode TLE:
-        # if sum(nums[:3]) > target:
-        #     return sum(nums[:3])
-        # elif sum(nums[-3:]) < target:
-        #     return sum(nums[-3:])
-        
+        # define variables
         closest = float('inf')
         res = 0
-        for i in range(len(nums) - 2):
-            l = i + 1
-            r = len(nums) - 1
-            #while l < r:
-            # changing while condition for python TLE
+        # iterate over all numbers
+        for i in range(len(nums)):
+            # define l and r
+            l, r = i + 1, len(nums) - 1
             while l < r:
+                # distance condition
                 if abs(nums[i] + nums[l] + nums[r] - target) < closest:
                     closest = abs(nums[i] + nums[l] + nums[r] - target)
                     res = nums[i] + nums[l] + nums[r]
+
+                # two pointer method of shifting l and r
                 if nums[i] + nums[l] + nums[r] < target:
                     l += 1
                 elif nums[i] + nums[l] + nums[r] > target:
                     r -= 1
                 else:
-                    res = target
-                    break
+                    # we have hit the target, and can return
+                    return nums[i] + nums[l] + nums[r]
         return res
-                        
-                    
+
