021164592003e118cd47bb0b8d172ad6a1cefb80
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-30 00:14:33 -0500

    Time: 39 ms (57.19%), Space: 13.8 MB (56.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,10 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        insert_index = 0
-        for i in range(3):
-            for r in range(len(nums)):
-                if nums[r] == i:
-                    nums[insert_index], nums[r] = nums[r], nums[insert_index]
-                    insert_index += 1            
+        # insert_index = 0
+        # for i in range(3):
+        #     for r in range(len(nums)):
+        #         if nums[r] == i:
+        #             nums[insert_index], nums[r] = nums[r], nums[insert_index]
+        #             insert_index += 1
         
+        #dutch flag method:
+        l, r = 0, len(nums) - 1
+        curr = 0
+        while curr <= r:
+            if nums[curr] == 0:
+                nums[curr], nums[l] = nums[l], nums[curr]
+                l += 1
+                curr += 1
+            elif nums[curr] == 1:
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[r] = nums[r], nums[curr]
+                r -= 1        
