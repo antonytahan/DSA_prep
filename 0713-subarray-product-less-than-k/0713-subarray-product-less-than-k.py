0c85885237c684c47ac64e9c1b438f8c1cdd95ff
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-30 15:48:41 -0500

    Time: 697 ms (87.04%), Space: 16.5 MB (97.32%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,21 +1,22 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # define variables
         product = 1
-        res = []
-        l = 0
         count = 0
+        l = 0
+        # if product is 1 or 0, then answer must be 0 since product < k
         if k <= 1:
             return 0
+        # iterate through numbers
         for r in range(len(nums)):
+            # adjust new product value
             product *= nums[r]
+            # if product gets too big
             while product >= k and l < len(nums):
+                #shift left pointer and adjust product
                 product /= nums[l]
                 l += 1
-            lst = []
-            # for i in range(r, l - 1, -1):
-            #     count += 1
-            #     lst.append(nums[i])
-            #     res.append(lst)
-            count += r - l + 1
+            # for every new valid number we can multiply with, there are r - l + 1 new subarrays that can be created
+            count += r - l + 1 
         return count
         
