864e7c4477e4b1ca0ab48c7b369cfb71513777de
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-01-30 15:30:47 -0500

    Time: 1254 ms (44.03%), Space: 13.9 MB (50.64%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,14 +1,23 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        # sort in order to implement a two pointer approach
         nums.sort()
+        # define variables
         count = 0
-        for i in range(len(nums) - 1):
+        # loop through all possible first numbers
+        for i in range(len(nums) - 2):
+            # define l and r pointers
             l, r = i + 1, len(nums) - 1
+            # two pointer approach
             while l < r:
-                if nums[l] + nums[r] < target - nums[i]:
-                    count += r - l 
+                # if triplet is smaller than target
+                if nums[i] + nums[l] + nums[r] < target:
+                    # add r - l to count since last of the triplet can be r or any number between l and r since r is the biggest
+                    count += r - l
+                    # increment l after adjusting count
                     l += 1
                 else:
+                    # else r is too big, shrink r
                     r -= 1
         return count
             
