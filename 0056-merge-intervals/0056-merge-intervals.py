3dbf08c795503dfb6686eabc89dc4da81353434d
Antony Tahan <36029438+antonytahan@users.noreply.github.com>
2023-03-01 13:35:35 -0500

    Time: 157 ms (47.00%), Space: 18.1 MB (77.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -22,16 +22,20 @@ class Solution:
         #         res.append(intervals[i])
         # return res
         
-        
+        res = []
+        if not intervals:
+            return res
         intervals.sort()
-        res = [intervals[0]]
+        res.append(intervals[0])
         for i in range(1, len(intervals)):
-            # does it start before the previous interval ends?
             if intervals[i][0] <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], intervals[i][1])
+                res[-1][1] = max(intervals[i][1], res[-1][1])
             else:
                 res.append(intervals[i])
         return res
+        
+            
+        
             
             
         
